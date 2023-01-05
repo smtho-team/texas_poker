2a582e87b06a155d1db6ac3afe5c18e6a11a52a6
mac <>
2023-01-05 13:30:05 +0800

    config文件更改

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
-/* eslint-env node */
-
 module.exports = {
   plugins: {
-    'postcss-px-to-viewport': {
-      viewportWidth: 360,
+    // postcss-pxtorem 插件的版本需要 >= 5.0.0
+    'postcss-pxtorem': {
+      rootValue: 37.5, // 1rem 的大小
+      // rootValue({ file }) { // 判断是否是vant的文件 如果是就使用 37.5为根节点字体大小
+      //   // 否则使用75 因为vant使用的设计标准为375 但是市场现在的主流设置尺寸是750
+      //   return file.indexOf('vant') !== -1 ? 37.5 : 75;
+      // },
+      // 配置哪些文件中的尺寸需要转化为rem *表示所有的都要转化
+      propList: ['*'],
     },
   },
 };
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 const webpack = require("webpack")
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
+
 module.exports = defineConfig({
   // transpileDependencies: true,
-  devServer: {
-    // https: true
-  },
   configureWebpack: {
     resolve: {
       extensions: ['.ts'],
@@ -30,5 +29,26 @@ module.exports = defineConfig({
       Web3: 'Web3',
       SimplexNoise: 'simplex-noise'
     }
+  },
+  chainWebpack: (config) => {
+    config.resolve.symlinks(true) // 修复热更新失效
+  },
+  css: {
+    // extract: false
+  },
+  devServer: {
+    https: true,
+    allowedHosts: 'all',
+    // https: true,
+    proxy: {//配置跨域
+      '/may': {
+          target: 'https://webapi.nftscan.com',
+          // ws: true,
+          changeOrigin: true,//允许跨域
+          pathRewrite: {
+              '^/may': '/may'//请求的时候使用这个api就可以
+          }
+      }
   }
+}
 })
